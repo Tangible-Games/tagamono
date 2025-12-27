998893628cde800b1e95d7e55b0e119ceb7dcfa5
Dmitrii Tokarev <truvor.skameikin@gmail.com>
2025-12-27 12:00:52 +0100

    Adds SDL3 as dependency

--- demos/psp_stereo/psp_stereo_main.cpp
+++ demos/psp_stereo/psp_stereo_main.cpp
@@ -1,8 +1,35 @@
+#include "SDL3/SDL.h"
+
 #include "symphony_lite/vector3d.hpp"
 
 #include <iostream>
 
+static const int kScreenWidth = 480;
+static const int kScreenHeight = 272;
+bool game_running = true;
+
+void mainloop() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_EVENT_QUIT:
+        game_running = false;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   std::cout << "Running PSP stereo demo" << std::endl;
+
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMEPAD);
+  SDL_CreateWindow("window", kScreenWidth, kScreenHeight, 0);
+
+  while (game_running) {
+    mainloop();
+  }
+
   return 0;
 }
